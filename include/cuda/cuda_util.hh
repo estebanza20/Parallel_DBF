5510be72214f2f7dfa1f2adf953d0156d8fdde7b
Esteban Zamora <estebanzacr.20@gmail.com>
2018-01-07 23:27:10 -0600

    Fixed inline CUDA device functions file location

--- include/cuda/cuda_util.hh
+++ include/cuda/cuda_util.hh
@@ -3,17 +3,14 @@
 
 #include "cuda_common.hh"
 
-__device__ __forceinline__ float norm_l1(const float& a);
-__device__ __forceinline__ float norm_l1(const float2& a);
-__device__ __forceinline__ float norm_l1(const float3& a);
-__device__ __forceinline__ float norm_l1(const float4& a);
+__device__ __forceinline__ float norm_l1(const float& a)  { return ::fabs(a); }
+__device__ __forceinline__ float norm_l1(const float2& a) { return ::fabs(a.x) + ::fabs(a.y); }
+__device__ __forceinline__ float norm_l1(const float3& a) { return ::fabs(a.x) + ::fabs(a.y) + ::fabs(a.z); }
+__device__ __forceinline__ float norm_l1(const float4& a) { return ::fabs(a.x) + ::fabs(a.y) + ::fabs(a.z) + ::fabs(a.w); }
 
-__device__ __forceinline__ float norm_l2(const float2& a);
-__device__ __forceinline__ float norm_l2(const float3& a);
-__device__ __forceinline__ float norm_l2(const float4& a);
-
-// __device__ float3 gpuMatElemToFloat(const uchar3 elem);
-// __device__ uchar3 floatToGpuMatElem(const float3 val);
+__device__ __forceinline__ float norm_l2(const float2& a) { return ::dot(a,a);}
+__device__ __forceinline__ float norm_l2(const float3& a) { return ::dot(a,a);}
+__device__ __forceinline__ float norm_l2(const float4& a) { return ::dot(a,a);}
 
 __device__ float4 gpuMatElemToFloat(const uchar4 elem);
 __device__ uchar4 floatToGpuMatElem(const float4 val);
--- src/cuda/util.cu
+++ src/cuda/util.cu
@@ -1,37 +1,5 @@
 #include "cuda/cuda_util.hh"
 
-__device__ __forceinline__ float norm_l1(const float& a)  { return ::fabs(a); }
-__device__ __forceinline__ float norm_l1(const float2& a) { return ::fabs(a.x) + ::fabs(a.y); }
-__device__ __forceinline__ float norm_l1(const float3& a) { return ::fabs(a.x) + ::fabs(a.y) + ::fabs(a.z); }
-__device__ __forceinline__ float norm_l1(const float4& a) { return ::fabs(a.x) + ::fabs(a.y) + ::fabs(a.z) + ::fabs(a.w); }
-
-__device__ __forceinline__ float norm_l2(const float2& a) { return ::dot(a,a);}
-__device__ __forceinline__ float norm_l2(const float3& a) { return ::dot(a,a);}
-__device__ __forceinline__ float norm_l2(const float4& a) { return ::dot(a,a);}
-
-
-// __device__
-// float3 gpuMatElemToFloat(const uchar3 elem)
-// {
-//   float3 r;
-//   r.x = elem.x/255.0f;
-//   r.y = elem.y/255.0f;
-//   r.z = elem.z/255.0f;
-//   return r;
-// }
-
-
-// __device__
-// uchar3 floatToGpuMatElem(const float3 val)
-// {
-//   uchar3 r;
-//   r.x = __saturatef(val.x)*255;
-//   r.y = __saturatef(val.y)*255;
-//   r.z = __saturatef(val.z)*255;
-//   return r;
-// }
-
-
 __device__
 float4 gpuMatElemToFloat(const uchar4 elem)
 {
